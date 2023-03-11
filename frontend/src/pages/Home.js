f5b42287dd17d4ae148d5384775f39de40ed16b5
sevenas <sevena_sandhu@sfu.ca>
2023-03-11 14:01:05 -0800

    added css changes to course pages

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
+import './css/home.css';
 
 const Home = () => {
 
   const [courses, setCourses] = useState([]);
+  const colours = ["#EDAA6C", "#EA713D", "#D83E27", "#152B53", "#1F6E8E", "#2C8B98", "#82A8A0"]
 
   const headers = {
     "Content-Type": "application/json"
@@ -25,16 +27,33 @@ const Home = () => {
       setCourses(obj.Items);
     }
     getData();
-    // console.log(courses)
   })
+
+  console.log(courses);
   
   return(
     <>
       <title>Home</title>
-      <h1>Courses</h1>
-      {/* <h2>Note: below are 2 items in a Dynamo table, fetched through Lambda function API</h2> */}
-      {courses.map(course => (<div><h4>{course.courseName.S}</h4><p>{course.description.S}</p></div>))}
+      <div className="wrapper">
+        <h1>Courses</h1>
+        <div className="separator"></div>
+        <div className="courses-container">
+          {/* <h2>Note: below are 2 items in a Dynamo table, fetched through Lambda function API</h2> */}
+          {courses.map((course, index) => (
+            <div className="course-item">
+              <div className="course-header" style={{background: `${colours[index]}`}}></div>
+              <div className="course-item-text">
+                <h4>{course.courseName.S}</h4>
+                <p className="course-desc">{course.description.S}</p> 
+                <div className="link-price">
+                  <p className="price">{course.price.S}</p>
+                  {/* <a className="link">Learn More</a> */}
+                </div>
+              </div>
+            </div>))}
+        </div>
       {/* <p>{JSON.stringify(courses)}</p> */}
+      </div>
     </>
   )
 };
