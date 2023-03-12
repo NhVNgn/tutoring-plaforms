c3331337ce74fe7d506c3f7a9bebd4231e26ff39
NhVNgn <limou@LAPTOP-2EC8MA82>
2023-03-11 22:06:12 -0800

    added registration call from ui and fix rendering issue in home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,20 +6,18 @@ const Home = () => {
   const [courses, setCourses] = useState([]);
   const colours = ["#EDAA6C", "#EA713D", "#D83E27", "#152B53", "#1F6E8E", "#2C8B98", "#82A8A0"]
 
-  const headers = {
-    "Content-Type": "application/json"
-  };
-
-    useEffect(() => {
+  useEffect(() => {
     const getData = async () => {
       const response = await fetch("https://56j70ao9r7.execute-api.us-east-1.amazonaws.com/dev", {
         method: 'POST',
-        headers: headers,
+        headers: {
+          "Content-Type": "application/json"
+        },
         redirect: 'follow'
       });
-        // .then(response => response.text())
-        // .then(result => alert(JSON.parse(result).body))
-        // .catch(error => console.log('error', error))
+      // .then(response => response.text())
+      // .then(result => alert(JSON.parse(result).body))
+      // .catch(error => console.log('error', error))
 
       const data = await response.json();
       const obj = JSON.parse(data.body);
@@ -27,11 +25,11 @@ const Home = () => {
       setCourses(obj.Items);
     }
     getData();
-  })
+  }, [])
 
   console.log(courses);
-  
-  return(
+
+  return (
     <>
       <title>Home</title>
       <div className="wrapper">
@@ -40,11 +38,11 @@ const Home = () => {
         <div className="courses-container">
           {/* <h2>Note: below are 2 items in a Dynamo table, fetched through Lambda function API</h2> */}
           {courses.map((course, index) => (
-            <div className="course-item">
-              <div className="course-header" style={{background: `${colours[index]}`}}></div>
+            <div className="course-item" key={index}>
+              <div className="course-header" style={{ background: `${colours[index]}` }}></div>
               <div className="course-item-text">
                 <h4>{course.courseName.S}</h4>
-                <p className="course-desc">{course.description.S}</p> 
+                <p className="course-desc">{course.description.S}</p>
                 <div className="link-price">
                   <p className="price">{course.price.S}</p>
                   {/* <a className="link">Learn More</a> */}
@@ -52,10 +50,10 @@ const Home = () => {
               </div>
             </div>))}
         </div>
-      {/* <p>{JSON.stringify(courses)}</p> */}
+        {/* <p>{JSON.stringify(courses)}</p> */}
       </div>
     </>
   )
 };
 
-  export default Home;
+export default Home;
